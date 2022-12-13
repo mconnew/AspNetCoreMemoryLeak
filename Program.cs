0394263f8e195c82fb54a9e0b17c3aff91c619af
Matt Connew <mconnew@users.noreply.github.com>
2022-12-13 13:37:38 -0800

    Basic memory leak demonstration

---
+++ Program.cs
@@ -0,0 +1,79 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace BasicAspNetCoreHelloWorld
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                if (Debugger.IsAttached)
+                {
+                    Debugger.Break(); // Capture heap snapshot
+                }
+                await RunStressAsync(100);
+                GC.Collect(2);
+                GC.Collect(2);
+            }
+        }
+
+        private static async Task RunStressAsync(int runCount)
+        {
+            for (int i = 0; i < runCount; i++)
+            {
+                await RunStressRound2();
+            }
+        }
+
+        private static async Task RunStressRound1()
+        {
+            var builder = WebApplication.CreateBuilder(Array.Empty<string>());
+            builder.WebHost.UseKestrel(options =>
+            {
+                options.ListenLocalhost(8443);
+            });
+            builder.Logging.ClearProviders();
+            var app = builder.Build();
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello world!");
+            });
+            await app.StartAsync();
+            await app.StopAsync();
+        }
+
+        private static async Task RunStressRound2()
+        {
+            var host = WebHost.CreateDefaultBuilder()
+                .UseKestrel(options =>
+                {
+                    options.ListenLocalhost(8443);
+                })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                })
+                .UseStartup<Startup>()
+                .Build();
+            await host.StartAsync();
+            await host.StopAsync();
+        }
+
+        private class Startup
+        {
+            public void Configure(IApplicationBuilder app)
+            {
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Hello world!");
+                });
+            }
+        }
+    }
+}
